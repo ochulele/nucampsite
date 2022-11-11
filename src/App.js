02512c0f28bc2eb01875f17ba9d17152cfc0b865
Ochuko Ogwhoro <ochuciano@gmail.com>
2022-11-11 13:52:01 -0500

    AnimatedDisplayCard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path='contact' element={<ContactPage />} />
             <Route path='directory' element={<CampsitesDirectoryPage />} />
             <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+            <Route path='about' element={<AboutPage />} />
         </Routes>
         <Footer />
     </div>
